04b6a2f9c66ac48104f99ebeb9a5711d66be2e7c
Davidemauri00 <dadi.spyro@gmail.com>
2025-12-09 21:14:38 +0100

    ese 2.5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -31,6 +31,6 @@ console.log(teachers);
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.indexOf('Fabio') !== -1;
 console.log(isFabioPresent);
-
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
