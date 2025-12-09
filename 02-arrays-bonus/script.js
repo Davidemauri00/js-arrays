e7a07607944f5ff861c10d4ac97df16ae2b1a126
Davidemauri00 <dadi.spyro@gmail.com>
2025-12-09 21:09:02 +0100

    ese 2.3

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -19,13 +19,14 @@ console.log(reversedTeachers);
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
 for (let i = 0; i < teachers.length; i++){
-  if (teachers[i].length >== 5){
+  if (teachers[i].length >= 5){
     longNames.push(teachers[i]);
   }
 }
 console.log(longNames);
 // 3. Rimuovi 'Ed' dall'array teachers
-
+teachers.splice(teachers.indexOf('Ed'), 1);
+console.log(teachers);
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
